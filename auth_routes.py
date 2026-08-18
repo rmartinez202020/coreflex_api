c8f12af1fde75bce2801bc684f24b479c3a8919a
Roque Martinez <rmartinez@coreflexalliance.net>
2026-08-18 12:33:44 -0400

    Backend-Writes

--- auth_routes.py
+++ auth_routes.py
@@ -1,3 +1,6 @@
+
+
+
 # auth_routes.py
 
 from datetime import datetime, timedelta, timezone
@@ -455,6 +458,7 @@ def session_ping(
 @router.post("/logout")
 def logout(
     body: LogoutRequest,
+    request: Request,
     db: Session = Depends(get_db),
     current_user=Depends(get_current_user),
 ):
@@ -487,6 +491,19 @@ def logout(
             db.commit()
         else:
             db.commit()
+
+        # LOGS & ACTIVITY
+        # SECURITY -> LOGOUT SUCCESS
+        send_log(
+            user_id=current_user.id,
+            user_email=current_user.email,
+            category=LOG_CATEGORY_SECURITY,
+            action="LOGOUT",
+            status=LOG_STATUS_SUCCESS,
+            message="User logout successful",
+            ip_address=_get_request_ip(request),
+            user_agent=_get_request_user_agent(request),
+        )
 
         return {"ok": True, "detail": "Logged out successfully."}
 
